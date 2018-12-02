a68f8faa48a7a68d01506bbe91358d00812c5c38
Mike Price <idflyfish@hotmail.com>
2018-12-01 17:45:32 -0700

    initial commit

---
+++ PowerShell.Helpers.psm1
@@ -0,0 +1,32 @@
+<#
+ .Synopsis
+   A function which returns an ScriptBlock wrapped in retry logic. 
+
+ .Description
+   A function which returns an ScriptBlock wrapped in retry logic.
+
+ .Parameter Command
+  The ScriptBlock which will be wrapped in retry logic.
+
+ .Parameter Condition
+  A ScriptBlock with conditional logic, when true, will break out of the retry loop.
+
+ .Example
+#>
+
+function Get-CommandWithRetry($command, $condition) {
+  return { 
+    param([int]$retries, [int]$sleepSeconds)    
+    do {
+      $result = & $command      
+      if ($(& $condition $result) -eq $true) {
+        return $result
+        break
+      }
+      --$retries
+      Start-Sleep $sleepSeconds
+    } while ($retries -gt 0)
+
+    return $result
+  }
+}
